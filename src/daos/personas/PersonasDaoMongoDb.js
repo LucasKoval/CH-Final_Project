d5c34dcaa45e43fcaf95da2716a1874a096c0ae2
Lucas Koval <lucaskovaldev@gmail.com>
2022-05-25 14:49:00 -0300

    First commit

---
+++ src/daos/personas/PersonasDaoMongoDb.js
@@ -0,0 +1,15 @@
+import ContenedorMongoDb from "../../contenedores/ContenedorMongoDb.js"
+
+class PersonasDaoMongoDb extends ContenedorMongoDb {
+
+    constructor() {
+        super('personas', {
+            id: { type: String, required: true },
+            nombre: { type: String, required: true },
+            apellido: { type: Number, required: true },
+            edad: { type: Number, required: true },
+        })
+    }
+}
+
+export default PersonasDaoMongoDb
