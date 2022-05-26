7aa6da6f030f080fb1eee127de698e6b72e23a7a
LucasKoval <lucaskovaldev@gmail.com>
2022-05-26 09:22:39 -0300

    Fixing daos folder

--- src/daos/personas/PersonasDaoArchivo.js
+++
@@ -1,9 +0,0 @@
-import ContenedorArchivo from '../../TempContainers/ContenedorArchivo'
-
-class PersonasDaoArchivo extends ContenedorArchivo {
-  constructor(rutaDir) {
-    super(`${rutaDir}/personas.json`)
-  }
-}
-
-export default PersonasDaoArchivo
