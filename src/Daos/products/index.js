bcad5cf63e7c3b5b51f8598af1d6f971cb590ca5
LucasKoval <lucaskovaldev@gmail.com>
2022-05-27 17:48:51 -0300

    DB env added

--- src/Daos/products/index.js
+++ src/Daos/products/index.js
@@ -1,25 +1,25 @@
 let productDAO
 
-switch (process.env.DB) {
-  /*case 'json':
+switch (process.env.DB_ENV) {
+  case 'json':
     const { default: ProductDAOFileSystem } = await import('./ProductDAO_FileSystem.js')
     productDAO = new ProductDAOFileSystem('products')
+    console.log('Set FileSystem as Database for Products!')
     break
   case 'mongodb':
     const { default: ProductDAOMongoDB } = await import('./ProductDAO_MongoDB.js')
     productDAO = new ProductDAOMongoDB()
+    console.log('Set MongoDB as Database for Products!')
     break
   case 'firebase':
     const { default: ProductDAOFirebase } = await import('./ProductDAO_Firebase.js')
     productDAO = new ProductDAOFirebase()
+    console.log('Set Firebase as Database for Products!')
     break
   default:
     const { default: ProductDAOMemory } = await import('./ProductDAO_Memory.js')
     productDAO = new ProductDAOMemory()
-    break*/
-  default:
-    const { default: ProductDAOMongoDB } = await import('./ProductDAO_MongoDB.js')
-    productDAO = new ProductDAOMongoDB()
+    console.log('Set Internal Memory as Database for Products!')
     break
 }
 
--- src/config.js
+++ src/config.js
@@ -2,6 +2,26 @@ export default {
   fileSystem: {
     path: './DB',
   },
-  mongodb: {},
-  firebase: {},
+  mongodb: {
+    cnxStr:
+      'mongodb+srv://LucasKoval:10349@7610Xregm@coderhousedb-cluster.dzdyr.mongodb.net/test?retryWrites=true&w=majority',
+    options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  },
+  firebase: {
+    type: 'service_account',
+    project_id: 'ch-tpfinal-ecommerce',
+    private_key_id: '2ec1288e11b9d52fe4e8496e9fa3e6aa4bff4ede',
+    private_key:
+      '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDDcplSGLLhvaYp\nzH2/Rgm3JRqcYS8b0zS9QBQQ1ePxbdabPqv30jjXYC5cms4LW+UHSA8AV9QFU2Vl\nCsztpnK9Xkn8Q+a069g5mSzkTH5AdppfoAC24Wx5WYBOXK/R0Zc6Ww05v3bI1VLv\nc5XCFCeEEH98MtCP4DhxevnTAXB2BgAxUmza5gCl7aXf7Oe3cXxeXKC1O9yEDePH\nWjHQ3sRlVNpMjhHpZyGIe2OwBI/G5Xngvd/q5p5vKmQRbVjQF1aPDVVmDsMl6z5T\nAz92tNRHXdgKmluiU6szQ07AG7pJKPbJefrArva31B+bUPX1WVCudm686GiugJsQ\ncf5VRzrJAgMBAAECggEAL9B8Y9I5CPLdkMI/TSj0PYLtRK6d2L/N2jKv9gJttmW5\np9coOQOL9lzIWs1gdPTxTdu6F8pkyzzAxHxk8ArVbTCBBx8RdNdx2X14tw1joZ+i\nWpyJMDRr1jAoLIOOW9VbmuIllSgbBjzyAjSTwZwuoqdqmNbRoTEofdKpBY8HlUeq\nuyUCGITSimd9e8wFPfT/BsJf94vzgI4d+K1CCkYa8eCKrKGqcOnG1mipFo6bMsL4\ntOqWjRORuWQ6W0NBJhHLxqemEPG+MddkJyCJDUl119HVRvBd7xcrGA5/LBFuUljg\nlHfdxi1/V3SN6hb/PQZuGlPfsBrCvOOkryJV0GsGFQKBgQDyrJCmbijXox7dV1+0\nNACyajiYXG23zU9Uoa9agzx6x8B5+MtxcD+kNUY0hxtO5kxCHPnK4W25W6vVDHaC\nz1TBiCD5TzT+LMFJjE6404GgWTxpMGc6LD3TFnJZXzJF01WpjA6Do++ps+pjlutq\nFJo+vICFBp9FwwGXvaF4cx6ttwKBgQDOLiPG8Xl1eUA/Qos8lUWhUkazlDhS2cAR\nUGAmz44+0nRq2MKwhuHot3DoucDYPUCwXaLIi0xuAqZeXCJ3pfz55lf7cnTvZ4jt\nqZ4/vx5EmGiYwOngNmgzCpq2+GHCs9KFPzNy7Z/GB8twYwjuokXEKFAYPY0wyO/0\nchwUeSlbfwKBgQCO+ndbZ6ouqtAjjzg2/zCt7gGcW1DVq2SjM4QSayI2IPzZoGmV\nU5JbmHvdtQC08uFZTqMqErnILSfHpckpdyizwhXTbgSGAS58u+sh4JQOOqVqQHeB\ngdGHTkFfQMRo+YQOGhPvIaayMOvXc3MOlEbw0bVTKzyqKKQGmFbLleAsbwKBgQCH\nuN26RxjiGLgPLHnFnqmgYS3DZs2Q1GBXxWYWBkkn15flAd1fINszOxYrTKPCdMfR\nkUHn67KbNElaEiJqSqz1wAiosBaDxc7gS+itI90D4KPx8gPCoW4EM3XW/jg0V/U7\n23e9ruifDmCAEeE+XTm5v6PB4TrBI3S5oCgV3hA4dwKBgBiDWKr2bYD6rC9qC5BO\nSJPtdMSODCOADWDYYxgTefHVJJf9b4D45mp/BCQtH/cCo952xGhoxgQsg0CYJ8kc\nenf6bh7fCzNuAirJ2vxKvx9flZex+7edpti+D0Ronus09t6FNIHExYPs+blWcoK2\nmFR8/3ko9JHjbDcJPKcbACHQ\n-----END PRIVATE KEY-----\n',
+    client_email: 'firebase-adminsdk-f16cx@ch-tpfinal-ecommerce.iam.gserviceaccount.com',
+    client_id: '105327222147029034010',
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url:
+      'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-f16cx%40ch-tpfinal-ecommerce.iam.gserviceaccount.com',
+  },
 }
