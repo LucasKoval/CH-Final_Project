c10f7233cea27ca70b562bdc17320b9507cab154
LucasKoval <lucaskovaldev@gmail.com>
2022-09-14 19:17:58 -0300

    Flow updated

--- src/Middlewares/isAdmin.js
+++ src/Middlewares/isAdmin.js
@@ -1,7 +1,16 @@
-import dotenv from "dotenv";
-dotenv.config();
+import dotenv from 'dotenv'
+dotenv.config()
 
-export const isAdmin = (req, res, next) => {
-	if (req.user.email == process.env.ADMIN_EMAIL) next();
-	else res.status(401).json({ message: "No autorizado para realizar esta operación.", code: "not_authorized", status: 401 });
-};
+const isAdmin = (req, res, next) => {
+  if (req.user.email == process.env.ADMIN_EMAIL) next()
+  else
+    res
+      .status(401)
+      .json({
+        message: 'No autorizado para realizar esta operación.',
+        code: 'not_authorized',
+        status: 401,
+      })
+}
+
+export default isAdmin
--- src/Middlewares/isAuth.js
+++ src/Middlewares/isAuth.js
@@ -1,37 +1,39 @@
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
 
-export const isAuthenticated = (req, res, next) => {
-	const headerAuthorization = req.headers["authorization"] || req.headers["Authorization"] || "";
+const isAuth = (req, res, next) => {
+  const headerAuthorization = req.headers['authorization'] || req.headers['Authorization'] || ''
 
-	if (!headerAuthorization) {
-		return res.status(401).json({
-			message: "Debe enviar un Authorization Bearer token en los headers.",
-			code: "Authorization_token_required",
-		});
-	}
+  if (!headerAuthorization) {
+    return res.status(401).json({
+      message: 'Debe enviar un Authorization Bearer token en los headers.',
+      code: 'Authorization_token_required',
+    })
+  }
 
-	const token = headerAuthorization.split(" ")[1];
+  const token = headerAuthorization.split(' ')[1]
 
-	if (!token) {
-		return res.status(401).json({
-			message: "Token requerido",
-			code: "token_required",
-			status: 401,
-		});
-	}
+  if (!token) {
+    return res.status(401).json({
+      message: 'Token requerido',
+      code: 'token_required',
+      status: 401,
+    })
+  }
 
-	try {
-		req.user = jwt.verify(token, process.env.JWT_SECRET);
-		console.log("REQ USER >>>>>>>>", req.user);
-	} catch (error) {
-		return res.status(403).json({
-			message: "No autorizado. Token inválido.",
-			code: "not_authorized",
-			status: 403,
-		});
-	}
+  try {
+    req.user = jwt.verify(token, process.env.JWT_SECRET)
+    console.log('REQ USER >>>>>>>>', req.user)
+  } catch (error) {
+    return res.status(403).json({
+      message: 'No autorizado. Token inválido.',
+      code: 'not_authorized',
+      status: 403,
+    })
+  }
 
-	next();
-};
+  next()
+}
+
+export default isAuth
